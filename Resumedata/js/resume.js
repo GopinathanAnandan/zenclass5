8c688920ad689a9f7db378ea1d7af51979164d54
Gopinathan <gopinathan.anandan@gmail.com>
2024-05-24 06:42:45 +0530

    update

---
+++ Resumedata/js/resume.js
@@ -0,0 +1,50 @@
+const resume = {
+    details: {
+        name: "Gopinathan",
+        fatherName: "Anandan",
+        birthYear: 1996,
+        Degree: "B.E",
+        location: {
+            address: "8/22 Pudupet road,7th street",
+            place: "Tirupattur",
+            state: "Tamilnadu",
+            country: "India"
+        }
+    },
+    contact:{
+        mobile:9944601260,
+        gmail:"gopinathan.anandan@gmail.com"
+    },
+    education:{
+        sslc:{
+            name:"Velankanni matriculation higher secondary school",
+            location:"Bargur",
+            passingYear:2012,
+            percentage:92
+        },
+        hsc:{
+            name:"Sri vijay vidyalaya",
+            location:"Dharmapuri",
+            passingYear:2014,
+            percentage:90
+        },
+        ug:{
+            name:"Rajalakshmi Engineering College",
+            location:"chennai",
+            passingYear:2018,
+            department:"Mechanial",
+            percentage:77
+        }
+    },
+    skill:['excel','SQL','JavaScript'],
+    work:{
+        companyName:"Amazon",
+        designation:"Associate",
+        joiningDate:"2022-11-07",
+        roleType:"Non-Tech",
+        experienceInYaers:2
+    },
+    language:['Tamil','English','Telugu']
+}
+
+console.log(resume);
